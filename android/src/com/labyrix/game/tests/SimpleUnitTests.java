88c3c3ddcfc8fe8d1b5463752d9f7cd84961b09f
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2021-04-19 11:51:06 +0200

    libGDX gradle adjustments

---
+++ android/src/com/labyrix/game/tests/SimpleUnitTests.java
@@ -0,0 +1,14 @@
+package com.labyrix.game.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class SimpleUnitTests {
+
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(1, 1);
+    }
+
+}

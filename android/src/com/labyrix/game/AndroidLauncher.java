ec9c40baaa22946a5f74e9020150b398a9b1f01a
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2021-04-19 10:53:41 +0200

    Setup libGDX, Travis, and Sonarcloud

---
+++ android/src/com/labyrix/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.labyrix.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.labyrix.game.labyrixMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new labyrixMain(), config);
+	}
+}

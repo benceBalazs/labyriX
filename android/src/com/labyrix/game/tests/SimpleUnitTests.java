95ed2f9d0190cdb58de8e71b2c9fd247ac02f5be
Alex <alexanderja@edu.aau.at>
2021-05-02 16:21:08 +0200

    implemented method for trap-activation

--- android/src/com/labyrix/game/tests/SimpleUnitTests.java
+++ android/src/com/labyrix/game/tests/SimpleUnitTests.java
@@ -1,7 +1,7 @@
 package com.labyrix.game.tests;
 
+import com.labyrix.game.Trap;
 import static org.junit.Assert.assertEquals;
-
 import org.testng.annotations.Test;
 
 public class SimpleUnitTests {
@@ -9,6 +9,24 @@ public class SimpleUnitTests {
     @Test
     public void oneEqualsOne() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testingTrapRandomMethod(){
+        float probability = 0.5f;
+        Trap trap = new Trap(probability);
+        double count = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            boolean trapWasActivated = trap.isTrapActivated();
+            if (trapWasActivated){
+                count++;
+            }
+        }
+
+        count /= 1000;
+
+        assertEquals(probability, count, 0.2);
     }
 
 }
--- core/src/com/labyrix/game/Trap.java
+++ core/src/com/labyrix/game/Trap.java
@@ -1,15 +1,30 @@
 package com.labyrix.game;
 
+import java.util.Random;
+
 public class Trap {
     private float probability;
     private TrapEvent event;
-
 
     public Trap (float probability){
         this.probability = probability;
         this.event = new TrapEvent();
     }
 
+    public boolean isTrapActivated () {
+        boolean result = false;
+
+        Random random = new Random();
+        int randomInt = random.nextInt(101);
+
+        float randomFloat = (float) randomInt / 100;
+
+        if (randomFloat <= this.probability){
+            result = true;
+        }
+
+        return result;
+    }
 
     public float getProbability() {
         return probability;

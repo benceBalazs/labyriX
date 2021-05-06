5625f4d18d32cf3ec6b73409b946af165c6119e4
Alex <alexanderja@edu.aau.at>
2021-05-06 19:07:59 +0200

    deleted test testingTrapRandomMethod from android tests because it was on the wrong place

--- android/src/com/labyrix/game/tests/SimpleUnitTests.java
+++ android/src/com/labyrix/game/tests/SimpleUnitTests.java
@@ -1,36 +1,12 @@
 package com.labyrix.game.tests;
 
-import com.labyrix.game.Trap;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.*;
 
 public class SimpleUnitTests {
-
-    private float probability;
-    private Trap trap = new Trap(0.5f);
-
-    @Before
-    public void init(){
-        probability = 0.5f;
-        trap = new Trap(probability);
-    }
-
     @Test
-    public void testingTrapRandomMethod(){
-        float count = 0;
-        int loops = 10000;
-
-        for (int i = 0; i < loops; i++) {
-            boolean trapWasActivated = trap.isTrapActivated();
-            if (trapWasActivated){
-                count++;
-            }
-        }
-
-        count /= loops;
-        System.out.println(probability);
-
-        assertEquals(probability, count, 0.01);
+    public void twoEqualsTwo() {
+        assertEquals(2, 2);
     }
-
 }

af411bee5f94fcb255c566a4c348fd302741eba3
Alex <alexanderja@edu.aau.at>
2021-05-07 13:28:08 +0200

    test added which checks if constructor of TrapEvent works

--- core/src/test/TestTrapEvent.java
+++ core/src/test/TestTrapEvent.java
@@ -1,4 +1,24 @@
 package test;
 
+import com.labyrix.game.TrapEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class TestTrapEvent {
+
+    TrapEvent trapEvent;
+
+    @Before
+    public void init(){
+        this.trapEvent = new TrapEvent();
+    }
+
+    @Test
+    public void TrapEvent_createsTrapEventAndReturnsIt(){
+        assertNotNull(trapEvent);
+    }
+
 }

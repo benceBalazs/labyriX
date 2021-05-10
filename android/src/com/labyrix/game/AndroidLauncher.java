bca9b633d80c0b7c64b9df119343bc63b3b9a880
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2021-05-11 00:27:06 +0200

    [#44] LabyrixMain complete overhaul

--- android/src/com/labyrix/game/AndroidLauncher.java
+++ android/src/com/labyrix/game/AndroidLauncher.java
@@ -10,6 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new LabyrixMain(), config);
+		initialize(new LabyrixMain().getINSTANCE(), config);
 	}
 }
--- core/src/com/labyrix/game/LabyrixMain.java
+++ core/src/com/labyrix/game/LabyrixMain.java
@@ -1,37 +1,88 @@
 package com.labyrix.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.labyrix.game.Models.Board;
-import com.labyrix.game.Models.Player;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.labyrix.game.Screens.GameScreen;
+import com.labyrix.game.Screens.JoinScreen;
+import com.labyrix.game.Screens.LoadingScreen;
+import com.labyrix.game.Screens.LobbyScreen;
+import com.labyrix.game.Screens.SplashScreen;
+import com.labyrix.game.Screens.TitleScreen;
 
 
 public class LabyrixMain extends Game {
-	public static LabyrixMain INSTANCE;
+	private static LabyrixMain INSTANCE;
+	private int WIDTH = 320;
+	private int HEIGHT = 480;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
-	private int cameraHeight = 400 * 4;
-	private int cameraWidth = 200 * 4;
+	private AssetManager assets;
+	private SplashScreen splashScreen;
+	private GameScreen gameScreen;
+	private LoadingScreen loadingScreen;
+	private TitleScreen titleScreen;
+	private LobbyScreen lobbyScreen;
+	private JoinScreen joinScreen;
+	private BitmapFont fontBig, fontMedium, fontMediumError;
 
-	public LabyrixMain(){
-		INSTANCE = this;
+	LabyrixMain(){
+
+	}
+
+	public static LabyrixMain getINSTANCE(){
+		if(INSTANCE == null){
+			INSTANCE = new LabyrixMain();
+		}
+		return INSTANCE;
 	}
 
 	@Override
 	public void create () {
+		HEIGHT = Gdx.graphics.getHeight();
+		WIDTH = Gdx.graphics.getWidth();
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(WIDTH, HEIGHT);
+		assets = new AssetManager();
+		splashScreen = new SplashScreen();
+		gameScreen = new GameScreen();
+		loadingScreen = new LoadingScreen();
+		titleScreen = new TitleScreen();
+		joinScreen = new JoinScreen();
+		lobbyScreen = new LobbyScreen();
+		initFonts();
+		this.setScreen(loadingScreen);
+	}
+
+	private void initFonts() {
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/specialElite.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		params.size = this.HEIGHT/14;
+		params.color = Color.WHITE;
+		fontBig = generator.generateFont(params);
+		params.size =  this.HEIGHT/18;
+		fontMedium = generator.generateFont(params);
+		params.color = Color.RED;
+		fontMediumError = generator.generateFont(params);
+		generator.dispose();
 	}
 
 	@Override
 	public void render () {
-
+		super.render();
 	}
 
+	@Override
 	public void dispose () {
 		batch.dispose();
+		assets.dispose();
+		splashScreen.dispose();
+		fontBig.dispose();
 	}
 
 }

9bdf4ef9f9a95d87e0053efb0be342d0b681d061
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2021-05-17 22:47:43 +0200

    [#44] Refactoring of Lobbyscreen and implementing TextFieldFilter for Inputs

--- core/src/com/labyrix/game/Screens/LobbyScreen.java
+++ core/src/com/labyrix/game/Screens/LobbyScreen.java
@@ -1,93 +1,197 @@
 package com.labyrix.game.Screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.esotericsoftware.kryonet.Client;
 import com.labyrix.game.LabyrixMain;
-import com.labyrix.game.Screens.GameScreen;
+import com.labyrix.game.Models.Player;
+import com.labyrix.game.Network.ClientNetworkHandler;
+
+import java.util.ArrayList;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.alpha;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeIn;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveBy;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.parallel;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
 
 public class LobbyScreen extends ScreenAdapter {
 
-        SpriteBatch batch;
-        Texture img;
+    private final LabyrixMain labyrixMain;
+    private Stage stage;
+    private Skin skinMedium;
+    private TextButton buttonPlay, playerProfile1, playerProfile2, playerProfile3, playerProfile4;
+    private Image logoImg, backgroundImg;
+    private Label lobbycode;
+    private ArrayList<Player> players = new ArrayList<>();
+    public ClientNetworkHandler clientNetworkHandler;
+    public Client client;
+    private String lobbyCodeReturn;
+    private JoinScreen joinScreen;
 
-        public static Texture backgroundTexture;
+    public LobbyScreen(JoinScreen joinScreen) {
+        this.joinScreen = joinScreen;
+        this.labyrixMain = LabyrixMain.getINSTANCE();
+        this.stage = new Stage(new FitViewport(labyrixMain.getWIDTH(), labyrixMain.getHEIGHT(), labyrixMain.getCamera()));
+        clientNetworkHandler = ClientNetworkHandler.getInstance();
+        clientNetworkHandler.addLobbyToClient(this);
+        client = clientNetworkHandler.getClient();
+    }
 
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
 
-        private Stage stage;
-        private Texture myTexture;
-        private TextureRegion myTextureRegion;
-        private TextureRegionDrawable myTexRegionDrawable;
-        private ImageButton button;
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
 
+    public Label getLobbycode() {
+        return lobbycode;
+    }
 
-        public LobbyScreen(){
-            myTexture = new Texture(Gdx.files.internal("Start.jpg"));
-            myTextureRegion = new TextureRegion(myTexture);
-            myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
-            button = new ImageButton(myTexRegionDrawable);
-            button.setPosition(400,200);
+    public void setLobbycode(String lobbycode) {
+        this.lobbycode.setText("Code: "+lobbycode);
+    }
 
-            stage = new Stage(new ScreenViewport());
-            stage.addActor(button);
-            Gdx.input.setInputProcessor(stage);
-
-
-            button.addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-
-
-                    //LabyrixMain.INSTANCE.setScreen((Screen) new GameScreen());
-                }
-
-                ;
-            });
-
-
-
-
-            batch = new SpriteBatch();
-            img = new Texture("schriftApp.jpg");
-
-
-            backgroundTexture = new Texture("background.png");
-
-
-
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+        stage.clear();
+        this.players = joinScreen.getPlayers();
+        this.lobbyCodeReturn = joinScreen.getLobbyCodeReturn();
+        this.skinMedium = new Skin();
+        this.skinMedium.addRegions(labyrixMain.getAssets().get("ui/uiskin.atlas", TextureAtlas.class));
+        this.skinMedium.add("default-font", labyrixMain.getFontMedium());
+        this.skinMedium.load(Gdx.files.internal("ui/uiskins.json"));
+        initScreen();
+        for (int i = 1; i <= 4; i++) {
+            addPlayerToLobby(i);
         }
-        @Override
-        public void render(float delta) {
-            ScreenUtils.clear(0, 0, 0, 0);
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            batch.begin();
-            batch.draw(img, 200, 800);
+    }
 
-            batch.draw(backgroundTexture,0,0,1500,750);
+    private void update(float delta) {
+        stage.act(delta);
+    }
 
-            batch.end();
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        update(delta);
+        stage.draw();
+    }
 
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
 
+    @Override
+    public void pause() {
 
-            stage.act(Gdx.graphics.getDeltaTime());
-            stage.draw();
+    }
 
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        stage.clear();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    public void addPlayerToLobby(Integer player) {
+        //TODO serverToClient --> JoinRespone in Lobby
+        switch (player){
+            case(1):    //playerProfile1 = new TextButton(players.get(0).getName(), skinMedium, "default");
+                playerProfile1 = new TextButton("Player1", skinMedium, "default");
+                playerProfile1.setSize(labyrixMain.getWIDTH()/2.8f, labyrixMain.getHEIGHT()/8f);
+                playerProfile1.setPosition(labyrixMain.getWIDTH()/2f - (playerProfile1.getWidth()/2) +labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/2f - (playerProfile1.getHeight()/2) + playerProfile1.getHeight()*1.875f);
+                playerProfile1.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+                stage.addActor(playerProfile1);
+                break;
+            case(2):    //playerProfile2 = new TextButton(players.get(1).getName(), skinMedium, "default");
+                playerProfile2 = new TextButton("Player2", skinMedium, "default");
+                playerProfile2.setSize(labyrixMain.getWIDTH()/2.8f, labyrixMain.getHEIGHT()/8f);
+                playerProfile2.setPosition(labyrixMain.getWIDTH()/2f - (playerProfile1.getWidth()/2) +labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/2f - (playerProfile1.getHeight()/2) + playerProfile1.getHeight()*0.625f);
+                playerProfile2.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+                stage.addActor(playerProfile2);
+                break;
+            case(3):    //playerProfile3 = new TextButton(players.get(2).getName(), skinMedium, "default");
+                playerProfile3 = new TextButton("Player3", skinMedium, "default");
+                playerProfile3.setSize(labyrixMain.getWIDTH()/2.8f, labyrixMain.getHEIGHT()/8f);
+                playerProfile3.setPosition(labyrixMain.getWIDTH()/2f - (playerProfile1.getWidth()/2) +labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/2f - (playerProfile1.getHeight()/2) - playerProfile1.getHeight()*0.625f);
+                playerProfile3.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+                stage.addActor(playerProfile3);
+                break;
+            case(4):    //playerProfile4 = new TextButton(players.get(3).getName(), skinMedium, "default");
+                playerProfile4 = new TextButton("Waiting...", skinMedium, "default");
+                playerProfile4.setColor(Color.GRAY);
+                playerProfile4.setSize(labyrixMain.getWIDTH()/2.8f, labyrixMain.getHEIGHT()/8f);
+                playerProfile4.setPosition(labyrixMain.getWIDTH()/2f - (playerProfile1.getWidth()/2) +labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/2f - (playerProfile1.getHeight()/2) - playerProfile1.getHeight()*1.875f);
+                playerProfile4.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+                stage.addActor(playerProfile4);
+                break;
+            default:    break;
         }
+    }
 
-        @Override
-        public void dispose() {
-            img.dispose();
-            backgroundTexture.dispose();
-        }
+    private void removePlayerFromLobby() {
+        //TODO serverToClient --> LeaveResponse
+    }
+
+    private void initScreen() {
+        Texture backgroundTex = labyrixMain.getAssets().get("background.png", Texture.class);
+        backgroundImg = new Image(backgroundTex);
+        backgroundImg.setPosition(0-Gdx.graphics.getWidth()/8f, 0-Gdx.graphics.getHeight()/8f);
+        backgroundImg.setSize(Gdx.graphics.getWidth()+2*Gdx.graphics.getWidth()/8f, Gdx.graphics.getHeight()+2*Gdx.graphics.getHeight()/8f);
+        backgroundImg.addAction(sequence(alpha(0),fadeIn(.5f)));
+
+        buttonPlay = new TextButton("Play", skinMedium, "default");
+        buttonPlay.setSize(labyrixMain.getWIDTH()/3.5f, labyrixMain.getHEIGHT()/10.0f);
+        buttonPlay.setPosition(labyrixMain.getWIDTH()/2f-buttonPlay.getWidth()/2-labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/6f);
+        buttonPlay.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+        buttonPlay.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                labyrixMain.setScreen(labyrixMain.getGameScreen());
+            }
+        });
+
+        Texture splashTex = labyrixMain.getAssets().get("lobby.png", Texture.class);
+        logoImg = new Image(splashTex);
+        logoImg.setSize(labyrixMain.getWIDTH()/3f, labyrixMain.getHEIGHT()/5f);
+        logoImg.setPosition(labyrixMain.getWIDTH()/2f-logoImg.getWidth()/2-labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/1.5f);
+        logoImg.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+
+        lobbycode = new Label("", skinMedium);
+        lobbycode.setText("Code: "+lobbyCodeReturn);
+        lobbycode.setSize(labyrixMain.getWIDTH()/5f, labyrixMain.getHEIGHT()/10.0f);
+        lobbycode.setPosition(labyrixMain.getWIDTH()/2f-lobbycode.getWidth()/2-labyrixMain.getWIDTH()/4f, labyrixMain.getHEIGHT()/1.75f);
+
+        stage.addActor(backgroundImg);
+        stage.addActor(logoImg);
+        stage.addActor(buttonPlay);
+        stage.addActor(lobbycode);
+    }
 }

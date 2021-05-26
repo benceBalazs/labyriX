c59a9cc31981f2d6dbfd530c131ae25609899a13
magrossmann <maxi.grossmann@hotmail.com>
2021-05-26 13:56:45 +0200

    Update skin in Bombdefuse and Refactor class

--- core/src/com/labyrix/game/Defusions/BombDefuse.java
+++ core/src/com/labyrix/game/Defusions/BombDefuse.java
@@ -3,6 +3,7 @@ package com.labyrix.game.Defusions;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -18,64 +19,55 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.labyrix.game.LabyrixMain;
 
 public class BombDefuse extends Actor {
+    private final LabyrixMain labyrixMain;
     private String bombcode;
     private String userinput;
     private Skin skin;
     private Table table;
     private TextButton buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonSeven, buttonEight, buttonNine, clear;
     private TextField userinputText;
-    /*private Stage stage;*/
-    private Window window;
     private boolean bombDefuse;
 
     public BombDefuse(float x, float y){
+        labyrixMain = LabyrixMain.getINSTANCE();
         bombDefuse = false;
         userinput = "";
         bombcode = "";
+
         for (int i = 0; i < 4; i++) {
             int randomNumber = (int)(Math.random()* 9 + 1);
             bombcode += randomNumber;
         }
 
-        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-
-        //stage = new Stage(new FitViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),cam));
+        skin = new Skin();
+        this.skin.addRegions(labyrixMain.getAssets().get("ui/uiskin.atlas", TextureAtlas.class));
+        this.skin.add("default-font", labyrixMain.getFontBig());
+        this.skin.load(Gdx.files.internal("ui/uiskins.json"));
 
         table = new Table(skin);
-        table.setX(x);
-        table.setY(y);
+        table.setX(x - 120);
+        table.setY(y + 100);
 
         userinputText = new TextField("userinput", skin);
         userinputText.setText(userinput);
         userinputText.setAlignment(1);
 
         buttonOne = new TextButton("1",skin);
-        buttonOne.getLabel().setFontScale(5);
         buttonTwo = new TextButton("2", skin);
-        buttonTwo.getLabel().setFontScale(5);
         buttonThree = new TextButton("3", skin);
-        buttonThree.getLabel().setFontScale(5);
         buttonFour = new TextButton("4",skin);
-        buttonFour.getLabel().setFontScale(5);
         buttonFive = new TextButton("5", skin);
-        buttonFive.getLabel().setFontScale(5);
         buttonSix = new TextButton("6", skin);
-        buttonSix.getLabel().setFontScale(5);
         buttonSeven = new TextButton("7",skin);
-        buttonSeven.getLabel().setFontScale(5);
         buttonEight = new TextButton("8", skin);
-        buttonEight.getLabel().setFontScale(5);
         buttonNine = new TextButton("9", skin);
-        buttonNine.getLabel().setFontScale(5);
         clear = new TextButton("clear", skin);
-        clear.getLabel().setFontScale(5);
 
         buttonOne.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 1;
                 updateUserInput();
-                System.out.println(userinput);
             }
 
         } );
@@ -84,7 +76,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 2;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonThree.addListener(new ClickListener() {
@@ -92,7 +83,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 3;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonFour.addListener(new ClickListener() {
@@ -100,7 +90,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 4;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonFive.addListener(new ClickListener() {
@@ -108,7 +97,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 5;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonSix.addListener(new ClickListener() {
@@ -116,7 +104,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 6;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonSeven.addListener(new ClickListener() {
@@ -124,7 +111,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 7;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonEight.addListener(new ClickListener() {
@@ -132,7 +118,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 8;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         buttonNine.addListener(new ClickListener() {
@@ -140,7 +125,6 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput += 9;
                 updateUserInput();
-                System.out.println(userinput);
             }
         } );
         clear.addListener(new ClickListener(){
@@ -148,11 +132,10 @@ public class BombDefuse extends Actor {
             public void clicked(InputEvent event, float x, float y) {
                 userinput = "";
                 updateUserInput();
-                System.out.println(userinput);
             }
         });
 
-        table.add((CharSequence)bombcode).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/7).getActor().setFontScale(5);
+        table.add((CharSequence)bombcode).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/7);
         table.add(userinputText).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/7);
         table.add(clear).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/7);
         table.row();
@@ -167,43 +150,7 @@ public class BombDefuse extends Actor {
         table.add(buttonSeven).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/6);
         table.add(buttonEight).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/6);
         table.add(buttonNine).size(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/6);
-
-        //Gdx.input.setInputProcessor(stage);
     }
-
-
-    public boolean defuseBomb() throws InterruptedException{
-        System.out.println(bombDefuse);
-        //stage.addActor(table);
-        /*
-        render();
-        clearStage();
-        double time = System.currentTimeMillis();
-        while (System.currentTimeMillis() < time + 10000){
-            if (userinput.equals(bombcode)){
-                bombDefuse = true;
-            }
-            else bombDefuse = false;
-            System.out.println(bombDefuse);
-            Thread.sleep(100);
-        }*/
-        return bombDefuse;
-    }
-    /*
-    public void render(){
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-    }
-
-    public void clearStage(){
-        Timer timer = new Timer();
-        timer.scheduleTask(new Timer.Task() {
-            @Override
-            public void run() {
-                stage.clear();
-            }
-        },8);
-    }*/
 
     public boolean bombResult(){
         if (userinput.equals(bombcode)){

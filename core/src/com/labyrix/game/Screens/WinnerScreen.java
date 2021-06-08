f2e6ea50ec8f1756f83e1f67da8b407061a9df6c
SophieGutjahr <angutjahr@edu.aau.at>
2021-06-08 10:40:11 +0200

    add Variable, Methode render and Methode show

--- core/src/com/labyrix/game/Screens/WinnerScreen.java
+++ core/src/com/labyrix/game/Screens/WinnerScreen.java
@@ -1,26 +1,48 @@
 package com.labyrix.game.Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.labyrix.game.LabyrixMain;
 import com.labyrix.game.Models.Board;
 
 public class WinnerScreen implements Screen {
-    private LabyrixMain labyrixMain;
-    private SpriteBatch batch;
-    private Board board;
+    private final LabyrixMain labyrixMain;
+    private Stage stage;
+    private Skin skin;
+    private Image backgroundImg;
+    private TextButton buttonBackToTitleScreen;
+    private Image logoImg;
 
     public WinnerScreen() {
         this.labyrixMain= LabyrixMain.getINSTANCE();
+        this.stage = new Stage(new FitViewport(labyrixMain.getWIDTH(), labyrixMain.getHEIGHT(), labyrixMain.getCamera()));
     }
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(stage);
+        stage.clear();
 
+        this.skin = new Skin();
+        this.skin.addRegions(labyrixMain.getAssets().get("ui/uiskin.atlas", TextureAtlas.class));
+        this.skin.add("default-font", labyrixMain.getFontBig());
+        this.skin.load(Gdx.files.internal("ui/uiskins.json"));
+        //Methode Call
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override

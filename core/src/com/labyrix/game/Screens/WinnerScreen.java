063f2a159566dabaddde1e678f9fd10ab0e614c6
SophieGutjahr <angutjahr@edu.aau.at>
2021-06-08 10:30:47 +0200

    Added variable and constructor in winnerScreen

---
+++ core/src/com/labyrix/game/Screens/WinnerScreen.java
@@ -0,0 +1,52 @@
+package com.labyrix.game.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.labyrix.game.LabyrixMain;
+import com.labyrix.game.Models.Board;
+
+public class WinnerScreen implements Screen {
+    private LabyrixMain labyrixMain;
+    private SpriteBatch batch;
+    private Board board;
+
+    public WinnerScreen() {
+        this.labyrixMain= LabyrixMain.getINSTANCE();
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+
+}

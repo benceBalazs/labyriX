d496444e9eed93d92f8230b8f21f9848cb6affd5
magrossmann <maxi.grossmann@hotmail.com>
2021-06-17 15:53:35 +0200

    Implementation Uncoverbutton clickevent

--- core/src/com/labyrix/game/TurnLogic.java
+++ core/src/com/labyrix/game/TurnLogic.java
@@ -24,6 +24,7 @@ public class TurnLogic {
     private Player player;
     private boolean turnDone;
     private Client client;
+    private UncoverRender uncoverRender;
     private ArrowActors arrowActors;
     private TrapRender trapRender;
     int animationCounter = 20;
@@ -43,6 +44,7 @@ public class TurnLogic {
         this.turnDone = false;
         this.player.turnValue = TurnValue.DICEROLL;
         this.trapRender = new TrapRender(camera);
+        this.uncoverRender = new UncoverRender(camera);
         arrowActors = new ArrowActors(camera);
 
         this.uncoverButton = new HudButton();
@@ -155,7 +157,12 @@ public class TurnLogic {
             }
 
             if (Gdx.input.justTouched() && Gdx.input.getX() >= uncoverButton.getxCoordinateButtonBegin() && Gdx.input.getX() <= uncoverButton.getxCoordinateButtonEnd() && Gdx.input.getY() <= Gdx.graphics.getHeight() - uncoverButton.getyCoordinateButtonBegin() && Gdx.input.getY() >= Gdx.graphics.getHeight() - uncoverButton.getyCoordinateButtonEnd()) {
-                //Logic for Uncover Cheat
+
+                Uncover uncover = new Uncover(player,players, client);
+                this.uncoverRender.setInputProcess();
+                this.uncoverRender.addToStage(uncover.getTable());
+                this.clicker = false;
+                this.uncoverButton.setActive(false);
             }
 
         } else {
@@ -168,6 +175,8 @@ public class TurnLogic {
 
             this.clicker = true;
             this.cheatButton.setActive(true);
+            this.uncoverRender.getStage().clear();
+            this.uncoverButton.setActive(true);
 
             if (this.player.getCurrentField().isWinField() == true) {
                 this.player.turnValue = TurnValue.WON;
@@ -481,6 +490,10 @@ public class TurnLogic {
         if (this.players.size() < 3) {
             this.players.add(player);
         }
+    }
+
+    public UncoverRender getUncoverRender() {
+        return uncoverRender;
     }
 
     public void setUncoverButton(HudButton uncoverButton) {

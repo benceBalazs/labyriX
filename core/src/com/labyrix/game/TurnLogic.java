96e7cbfe95e68f954b08616adb4f9023f5aa4142
Denies Lafleur <delafleur@edu.aau.at>
2021-06-19 11:42:27 +0200

    .

--- core/src/com/labyrix/game/TurnLogic.java
+++ core/src/com/labyrix/game/TurnLogic.java
@@ -461,7 +461,7 @@ public class TurnLogic {
                 }
             }
         } else {
-            throw new IllegalArgumentException(("Trapdefuse - Wrong TurnValue or Turn not done");
+            throw new IllegalArgumentException("Trapdefuse - Wrong TurnValue or Turn not done");
         }
     }
 
